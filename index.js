e4ac5e4e86de8c6302f1b28a5d8fbdd53ca3452f
Johannes Scharlach <johannes@shareiq.com>
2018-01-02 18:05:41 +0700

    feat: parse compact integers of all sorts

--- index.js
+++ index.js
@@ -1,7 +1,68 @@
 'use strict'
 
+const unitsToFactor = {
+  k: 1e3,
+  M: 1e6,
+  B: 1e9,
+  T: 1e12,
+}
+
+const withAbbrRegex = /^-?[\d.,]+\s?[a-zA-Z]$/
+function withAbbr(compactWithAbbr = '') {
+  const num = compactWithAbbr.slice(0, -1).trim()
+  const abbr = compactWithAbbr.slice(-1)
+
+  return parseInt(num * unitsToFactor[abbr], 10)
+}
+
+const scientificRegex = /^-?[\d,.]+e\d+$/
+function scientific(compactScientific = '') {
+  const [mult, exponent] = compactScientific.split('e')
+
+  return parseInt(mult.split(',').join('') * 10 ** exponent, 10)
+}
+
+const humanScientificRegex = /^-?[\d,.]+x10\^\d+$/
+function humanScientific(compactHumanScientific = '') {
+  return scientific(compactHumanScientific.split('x10^').join('e'))
+}
+
+const justNumberRegex = /^-?[\d,]+$/
+function justNumber(compactNumber) {
+  return parseInt(compactNumber.split(',').join(''), 10)
+}
+
+const infRegex = /^-?Infinity$/i
+function inf(infString = '') {
+  return infString.charAt(0) === '-' ? -Infinity : Infinity
+}
+
+const strategies = [
+  [withAbbrRegex, withAbbr],
+  [scientificRegex, scientific],
+  [humanScientificRegex, humanScientific],
+  [justNumberRegex, justNumber],
+  [infRegex, inf],
+]
+
 function fromCompactInteger(compact) {
-  return parseInt(compact, 10)
+  if (typeof compact === 'number') return parseInt(compact, 10)
+
+  if (typeof compact !== 'string') {
+    throw Error(
+      `fromCompactInteger only works on numbers and strings. You passed in ${typeof compact}.`
+    )
+  }
+
+  for (const [regex, strategy] of strategies) {
+    if (regex.test(compact)) {
+      return strategy(compact)
+    }
+  }
+
+  throw new Error(
+    `Could not determine a good strategy to dehumanize "${compact}".`
+  )
 }
 
 module.exports = {
--- index.test.js
+++ index.test.js
@@ -8,5 +8,35 @@ describe('dehumanize', () => {
     it('parses a small number', () => {
       expect(fromCompactInteger('23')).toBe(23)
     })
+
+    it('parses a number with k', () => {
+      expect(fromCompactInteger('183k')).toBe(183000)
+    })
+
+    it('parses "Infinity"', () => {
+      expect(fromCompactInteger('Infinity')).toBe(Infinity)
+    })
+
+    it('parses "-INFINITY"', () => {
+      expect(fromCompactInteger('-INFINITY')).toBe(-Infinity)
+    })
+
+    const numbers = [77e2, 77e4, 77e6, 77e8, 77e10, 77e12, 77e14, 77e16]
+
+    describe('humanized numbers with precision 1', () => {
+      numbers.forEach(num => {
+        const humanNum = humanize.compactInteger(num, 1)
+        it(`is able to interpret "${humanNum}"`, () => {
+          expect(fromCompactInteger(humanNum)).toBe(num)
+        })
+      })
+
+      numbers.map(num => num * -1).forEach(num => {
+        const humanNum = humanize.compactInteger(num, 1)
+        it(`is able to interpret "${humanNum}"`, () => {
+          expect(fromCompactInteger(humanNum)).toBe(num)
+        })
+      })
+    })
   })
 })
